637f4aa0c2fcd9217b0351eb3664942fb6406c09
Alex Szabo <szabbo87@gmail.com>
2020-11-16 16:34:41 +0100

    Base models created

---
+++ src/main/java/com/agilexpert/task/model/App.java
@@ -0,0 +1,26 @@
+package com.agilexpert.task.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class App {
+
+    @Id
+    @GeneratedValue
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+}
---
+++ src/main/java/com/agilexpert/task/model/User.java
@@ -0,0 +1,29 @@
+package com.agilexpert.task.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+
+}
